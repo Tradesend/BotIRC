1257a523dc9b41a0ce81273b5ba37aa4ff961b4c
tradesend <diego.moran-jurado@epitech.eu>
2015-04-28 09:33:14 +0200

    bot

---
+++ bot.c
@@ -0,0 +1,104 @@
+/*
+** bot.c for bot in /home/moran-_d/rendu/pathwar
+** 
+** Made by moran-_d
+** Login   <moran-_d@epitech.net>
+** 
+** Started on  Mon Apr 27 09:25:45 2015 moran-_d
+** Last update Mon Apr 27 11:33:15 2015 moran-_d
+*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#define MAXLINE 4096
+
+int my_connect(char *server, char *port, int *sockfd);
+int my_send(int sockfd, int debug, char *fmt, ...);
+int my_read(int sockfd, char *recvline, int debug);
+
+int main(int argc, char **argv) {
+  int sockfd, n, debug;
+  char recvline[MAXLINE+1];
+  char *cmd, *pos, *tm;
+  char server[] = "irc.pathwar.net";
+  char user[] = "ouiti_f";
+  char port[] = "6667";
+  char ignore[] = "#pathwar-fr :";
+  int ignoresize = sizeof(ignore) - 1;
+  int joined = 0;
+  debug = 0;
+
+  if (!my_connect(server, port, &sockfd))
+    {
+      printf("Failed to connect to %s.\n", server);
+      return (0);
+    }
+
+  my_send(sockfd, debug, "NICK %s\r\n", user);
+  sleep(1);
+  my_send(sockfd, debug, "USER %s 0 0 :%s \r\n", user, user);
+  sleep(2);
+  my_send(sockfd, debug, "JOIN #pathwar-fr\r\n");
+
+  while(1)
+    {
+      n = my_read(sockfd, recvline, debug);      
+      if (n > 0)
+	{
+	  recvline[n] = 0;
+	  pos = recvline;
+	  printf("IN: %s <<<\n", pos);
+	  if ((tm = strstr(pos, "PING")) != NULL)
+	    {
+	      tm = strstr(tm, ":") + 1;
+	      my_send(sockfd, debug, "PONG %s\r\n", tm);
+	    }
+	}
+      if (++joined <= 2)
+	my_send(sockfd, debug, "JOIN #pathwar-fr\r\n");
+    }
+  exit(0);
+}
+
+int my_connect(char *server, char *port, int *sockfd) {
+  struct addrinfo hints, *res;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  getaddrinfo(server, port, &hints, &res);
+
+  if ( (*sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
+    return 0;
+  }
+  if (connect(*sockfd, res->ai_addr, res->ai_addrlen) < 0 ) {
+    return 0;
+  }
+
+  return 1;
+}
+
+int my_send(int sockfd, int debug, char *fmt, ...) {
+  char buf[MAXLINE];
+  va_list ap;
+
+  memset(buf, 0, MAXLINE);
+  va_start(ap, fmt);
+  vsnprintf(buf, MAXLINE, fmt, ap);
+  va_end(ap);
+  printf("OUT: %s <<<\n", buf);
+  return send(sockfd, buf, strlen(buf), 0);
+}
+
+int my_read(int sockfd, char *recvline, int debug) {
+  int n;
+  n = read(sockfd, recvline, MAXLINE);
+  if (n > 0 && debug) {
+    printf("IN: %s <<<\n", recvline);
+  }
+  return n;
+}
---
+++ bot2.c
@@ -0,0 +1,106 @@
+/*
+** bot2.c for bot2 in /home/moran-_d/rendu/pathwar
+** 
+** Made by moran-_d
+** Login   <moran-_d@epitech.net>
+** 
+** Started on  Mon Apr 27 10:01:37 2015 moran-_d
+** Last update Mon Apr 27 10:10:49 2015 moran-_d
+*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+
+int conn;
+char sbuf[512];
+
+void raw(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(sbuf, 512, fmt, ap);
+  va_end(ap);
+  printf("<< %s", sbuf);
+  write(conn, sbuf, strlen(sbuf));
+}
+
+int main() {
+    
+  char *nick = "test";
+  char *channel = "#pathwar-fr";
+  char *host = "irc.epitech.net";
+  char *port = "6667";
+    
+  char *user, *command, *where, *message, *sep, *target;
+  int i, j, l, sl, o = -1, start, wordcount;
+  char buf[513];
+  struct addrinfo hints, *res;
+    
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  getaddrinfo(host, port, &hints, &res);
+  conn = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  connect(conn, res->ai_addr, res->ai_addrlen);
+    
+  raw("USER %s 0 0 :%s\r\n", nick, nick);
+  raw("NICK %s\r\n", nick);
+  raw("JOIN %s\r\n", channel);
+    
+  while ((sl = read(conn, sbuf, 512))) {
+    for (i = 0; i < sl; i++) {
+      o++;
+      buf[o] = sbuf[i];
+      if ((i > 0 && sbuf[i] == '\n' && sbuf[i - 1] == '\r') || o == 512) {
+	buf[o + 1] = '\0';
+	l = o;
+	o = -1;
+                
+	printf(">> %s", buf);
+                
+	if (!strncmp(buf, "PING", 4)) {
+	  buf[1] = 'O';
+	  raw(buf);
+	} else if (buf[0] == ':') {
+	  wordcount = 0;
+	  user = command = where = message = NULL;
+	  for (j = 1; j < l; j++) {
+	    if (buf[j] == ' ') {
+	      buf[j] = '\0';
+	      wordcount++;
+	      switch(wordcount) {
+	      case 1: user = buf + 1; break;
+	      case 2: command = buf + start; break;
+	      case 3: where = buf + start; break;
+	      }
+	      if (j == l - 1) continue;
+	      start = j + 1;
+	    } else if (buf[j] == ':' && wordcount == 3) {
+	      if (j < l - 1) message = buf + j + 1;
+	      break;
+	    }
+	  }
+                    
+	  if (wordcount < 2) continue;
+                    
+	  if (!strncmp(command, "001", 3) && channel != NULL) {
+	    raw("JOIN %s\r\n", channel);
+	  } else if (!strncmp(command, "PRIVMSG", 7) || !strncmp(command, "NOTICE", 6)) {
+	    if (where == NULL || message == NULL) continue;
+	    if ((sep = strchr(user, '!')) != NULL) user[sep - user] = '\0';
+	    if (where[0] == '#' || where[0] == '&' || where[0] == '+' || where[0] == '!') target = where; else target = user;
+	    printf("[from: %s] [reply-with: %s] [where: %s] [reply-to: %s] %s", user, command, where, target, message);
+	    raw("%s %s :%s", command, target, message); // If you enable this the IRCd will get its "*** Looking up your hostname..." messages thrown back at it but it works...
+	  }
+	}
+                
+      }
+    }
+        
+  }
+    
+  return 0;
+    
+}
